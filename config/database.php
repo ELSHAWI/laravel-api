5d64a18ce8ee66583e5899a650a047de4626e5bb
ELSHAWI <ahmedelshawy2012@gmail.com>
2025-05-26 04:47:53 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('postgresql://elshawi_api_user:BadaRZkNSdAzdqFTDiE7NLbhZH7FvaCY@dpg-d0pllk8dl3ps73au0tv0-a/elshawi_api'),
+            'host' => env('DB_HOST', 'dpg-d0pllk8dl3ps73au0tv0-a'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'elshawi_api'),
+            'username' => env('DB_USERNAME', 'elshawi_api_user'),
+            'password' => env('DB_PASSWORD', 'BadaRZkNSdAzdqFTDiE7NLbhZH7FvaCY'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
